3590a2a283a06534a6b4d471684289f3f9c55a77
Vangos <vangos@lightbuzz.com>
2020-06-19 17:39:11 +0300

    Initial commit.

---
+++ Assets/Mastering_Azure_Kinect/Scripts/Editor/UnityEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoadAttribute]
+public class UnityEnvironment : MonoBehaviour
+{
+    static UnityEnvironment()
+    {
+        EditorApplication.playModeStateChanged += LogPlayModeState;
+    }
+
+    private static void LogPlayModeState(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            // Copy files
+        }
+    }
+}
