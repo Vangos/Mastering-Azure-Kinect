76df4ca0199c41acaf0080a481d3c29ec2d7cdd6
Vangos <vangos@lightbuzz.com>
2020-06-22 19:17:57 +0300

    Added the Configuration sample.

--- Assets/Mastering_Azure_Kinect/Scripts/Azure_Kinect_Configuration.cs
+++ Assets/Mastering_Azure_Kinect/Scripts/Azure_Kinect_Configuration.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Microsoft.Azure.Kinect.Sensor;
 using UnityEngine;
 
 public class Azure_Kinect_Configuration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private FPS cameraFps = FPS.FPS30;
+    [SerializeField] private ImageFormat colorFormat = ImageFormat.ColorBGRA32;
+    [SerializeField] private ColorResolution colorResolution = ColorResolution.R1080p;
+    [SerializeField] private DepthMode depthMode = DepthMode.NFOV_Unbinned;
+    [SerializeField] private bool syncedImagesOnly = true;
+
+    private Device kinect;
+
+    private void Start()
     {
-        
+        int deviceCount = Device.GetInstalledCount();
+
+        Debug.Log($"Found {deviceCount} devices.");
+
+        if (deviceCount > 0)
+        {
+            kinect = Device.Open();
+
+            kinect.StartCameras(new DeviceConfiguration
+            {
+                CameraFPS = cameraFps,
+                ColorFormat = colorFormat,
+                ColorResolution = colorResolution,
+                DepthMode = depthMode,
+                SynchronizedImagesOnly = syncedImagesOnly
+            });
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (kinect != null)
+        {
+            using (Capture capture = kinect.GetCapture())
+            {
+
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (kinect != null)
+        {
+            kinect.StopCameras();
+            kinect.Dispose();
+        }
     }
 }
