c5366389070d401b70e4b2507b8077948f6f177f
Vangos Pterneas <vangos@lightbuzz.com>
2021-03-08 18:22:35 +0200

    Added an Azure Cognitive Services demo.

--- Assets/Mastering_Azure_Kinect/Scripts/Azure_Kinect_ComputerVision.cs
+++ Assets/Mastering_Azure_Kinect/Scripts/Azure_Kinect_ComputerVision.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,18 +9,23 @@ public class Azure_Kinect_ComputerVision : MonoBehaviour
 {
     [SerializeField] private KinectConfiguration _configuration;
     [SerializeField] private RawImage _image;
+    [SerializeField] private Transform _canvas;
+    [SerializeField] private GameObject _detectedObjectPrefab;
 
-    private const string _visionApiKey = "8991690980884ab6b2a8188c542a77c7";
-    private const string _visionEndpoint = "https://lightbuzzcomputervision.cognitiveservices.azure.com/";
+    private const string VisionApiKey = "8991690980884ab6b2a8188c542a77c7";
+    private const string VisionEndpoint = "https://lightbuzzcomputervision.cognitiveservices.azure.com/";
 
     private readonly KinectSensor _kinect = new KinectSensor();
-    private readonly ComputerVisionClient _azure = new ComputerVisionClient(new ApiKeyServiceClientCredentials(_visionApiKey))
+
+    private readonly ComputerVisionClient _azure = new ComputerVisionClient(new ApiKeyServiceClientCredentials(VisionApiKey))
     {
-        Endpoint = _visionEndpoint
+        Endpoint = VisionEndpoint
     };
 
+    private readonly List<DetectedObjectRectangle> _rectangles = new List<DetectedObjectRectangle>();
+
     private Texture2D _texture;
-    private bool _isRunning = false;
+    private bool _isProcessing = false;
 
     private void Start()
     {
@@ -38,9 +44,8 @@ public class Azure_Kinect_ComputerVision : MonoBehaviour
         if (frameData != null)
         {
             byte[] colorData = frameData.Color;
-            ushort[] depthData = frameData.Depth;
 
-            DetectObjects(colorData, depthData);
+            DetectObjects(colorData);
 
             _texture.LoadImage(colorData);
         }
@@ -49,23 +54,72 @@ public class Azure_Kinect_ComputerVision : MonoBehaviour
     private void OnDestroy()
     {
         _kinect.Stop();
+        _azure.Dispose();
+
+        foreach (DetectedObjectRectangle rectangle in _rectangles)
+        {
+            if (rectangle.isActiveAndEnabled)
+            {
+                Destroy(rectangle.gameObject);
+            }
+        }
+
+        _rectangles.Clear();
     }
 
-    private async void DetectObjects(byte[] colorData, ushort[] depthData)
+    private async void DetectObjects(byte[] colorData)
     {
-        if (_isRunning) return;
-        if (colorData == null || depthData == null) return;
+        if (_isProcessing) return;
+        if (!_kinect.IsRunning) return;
+        if (colorData == null) return;
 
         try
         {
-            _isRunning = true;
+            _isProcessing = true;
 
             using (Stream stream = new MemoryStream(colorData))
             {
                 DetectResult result = await _azure.DetectObjectsInStreamAsync(stream);
+
+                if (result.Objects.Count != _rectangles.Count)
+                {
+                    foreach (DetectedObjectRectangle rectangle in _rectangles)
+                    {
+                        Destroy(rectangle.gameObject);
+                    }
+
+                    _rectangles.Clear();
+
+                    foreach (DetectedObject item in result.Objects)
+                    {
+                        GameObject go = Instantiate(_detectedObjectPrefab, _canvas);
+                        DetectedObjectRectangle rectangle = go.GetComponent<DetectedObjectRectangle>();
+
+                        _rectangles.Add(rectangle);
+                    }
+                }
+
+                for (int i = 0; i < result.Objects.Count; i++)
+                {
+                    DetectedObject item = result.Objects[i];
+                    DetectedObjectRectangle rectangle = _rectangles[i];
+
+                    BoundingRect rect = item.Rectangle;
+
+                    int centerX = rect.X + rect.W / 2;
+                    int centerY = rect.Y + rect.H / 2;
+
+                    Vector2 center = new Vector2(centerX, centerY);
+                    Vector2 size = new Vector2(rect.W, rect.H);
+                    Vector3 position = _kinect.CoordinateMapper.MapColorToWorld(center);
+
+                    Debug.Log(position);
+
+                    rectangle.Load(item.ObjectProperty, center, size, position);
+                }
             }
 
-            _isRunning = false;
+            _isProcessing = false;
         }
         catch
         {
---
+++ Assets/Mastering_Azure_Kinect/Scripts/GUI/DetectedObjectRectangle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectedObjectRectangle : MonoBehaviour
+{
+    [SerializeField] private Image _border;
+    [SerializeField] private Text _textType;
+    [SerializeField] private Text _textDistance;
+
+    public void Load(string type, Vector2 center, Vector2 size, Vector3 position)
+    {
+        _textType.text = type;
+        _textDistance.text = position.z.ToString("N2") + "m";
+
+        _border.rectTransform.anchoredPosition = new Vector2(center.x, -center.y);
+        _border.rectTransform.sizeDelta = size;
+    }
+}
